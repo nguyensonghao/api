e18858afaf4c08164d0997eb35a610b2ada8228b
nguyensonghao <nguyensonghao974@gmail.com>
2016-01-28 09:38:00 +0700

    sua config database

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'api_mazii',
-			'username'  => 'api_mazii',
+			'database'  => 'admin_api',
+			'username'  => 'admin_api',
 			'password'  => 'Eup.mobi@2015',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
