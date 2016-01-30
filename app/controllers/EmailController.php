6ecf4d0d72fd404df45f13949be0c4300654e23b
nguyensonghao <nguyensonghao974@gmail.com>
2016-01-30 08:36:40 +0700

    sua ham gui email

--- app/controllers/EmailController.php
+++ app/controllers/EmailController.php
@@ -5,28 +5,17 @@ require public_path() . "/libs/PHPMailer/class.smtp.php";
 class EmailController extends BaseController {
 
     public function sendMailActive ($keyActive, $email) {
-    	$mail = new PHPMailer();
-    	$mail->IsSMTP();
-        $mail->SMTPDebug  = 0;
-        $mail->Debugoutput = "html";
-        $mail->Host       = "box308.bluehost.com";
-        $mail->Port       = 465;
-        $mail->SMTPSecure = "ssl";
-        $mail->SMTPAuth   = true;
-        $mail->CharSet = "UTF-8";
-        $mail->Username   = "support@mazii.net";
-        $mail->Password   = "{i(R+g@p9J%T";
-        $mail->SetFrom("support@mazii.net", "Từ điển Mazii");
-        $mail->AddReplyTo("support@mazii.net","Từ điển Mazii");
-        $mail->AddAddress($email, $email);
-        $mail->Subject = "Kích hoạt tài khoản";
-        $mail->MsgHTML("Chào bạn " .$email. " <br>
+        $contentEmail = "Chào bạn " .$email. " <br>
         Bạn đã đăng ký thành công tài khoản trên Mazii. <br>
         Đây là thông tin tài khoản của bạn. <br>
         Email : " .$email. "<br>
         Xin hãy click vào link dưới đây để xác nhận tài khoản email của bạn. <br>
-        http://api.mazii.net/api/active/" . $keyActive);
-        return $mail->Send();
+        http://api.mazii.net/api/active/" . $keyActive;
+
+        return Mail::queue('email', array('firstname'=> 'Từ điển Mazii'), function($message) {
+            $message->to($email, $email)->subject('Kích hoạt tài khoản')
+            ->setBody($contentEmail);
+        });
     }
 
     public function sendMailActiveSuccess ($email) {
