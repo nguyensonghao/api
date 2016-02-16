47a1e82bd8d985c5ac060780967d738ff633820c
nguyensonghao <nguyensonghao974@gmail.com>
2016-02-16 17:52:04 +0700

    them file migrate

---
+++ app/database/migrations/2016_02_16_104940_create_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('catagory', function ($table) {
+			$table->increments('categoryId');
+	        $table->string('categoryName');
+	        $table->integer('userId');
+	        $table->timestamps();
+	    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
