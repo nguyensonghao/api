0ecc5bb4798ef5cef2e88c86b41af4627df534b4
nguyensonghao <nguyensonghao974@gmail.com>
2016-02-19 08:13:57 +0700

    them database mazii

--- app/database/migrations/2016_02_19_011122_create_mazii_table.php
+++ app/database/migrations/2016_02_19_011122_create_mazii_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoryTable extends Migration {
+class CreateMaziiTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,12 @@ class CreateCategoryTable extends Migration {
 	 */
 	public function up()
 	{
-		//
+		Schema::create('note', function ($table) {
+			$table->increments('maziiId');
+	        $table->integer('userId');
+	        $table->integer('status');
+	        $table->timestamps();
+	    });
 	}
 
 	/**
