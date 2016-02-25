f4bf574802919ecd331f9afa861fb3507f36be34
nguyensonghao <nguyensonghao974@gmail.com>
2016-02-25 09:13:09 +0700

    sua theo thoi gian chuan

--- app/controllers/DateController.php
+++ app/controllers/DateController.php
@@ -3,8 +3,9 @@
 class DateController {
 
 	public function checkInvalidDate ($timeClient) {
+	    date_default_timezone_set('Europe/London');
 		$date = new DateTime();
-	    $timeServer = $date->getTimestamp();
+		$timeServer = $date->getTimestamp();
 	    $minus = $timeServer - $timeClient;
 	    if ($minus > 60) 
 	    	return false;
--- app/routes.php
+++ app/routes.php
@@ -112,3 +112,23 @@ Route::get('demo', function () {
 	$timeServer = $date->getTimestamp();
 	echo $timeServer;
 });
+
+Route::get('demo2', function () {
+	function get_url_contents($url) {
+		$crl = curl_init();
+		curl_setopt($crl, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322)');
+		curl_setopt($crl, CURLOPT_URL, $url);
+		curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, 5);
+		$ret = curl_exec($crl);
+		curl_close($crl);
+		return $ret;
+	}
+	$json = get_url_contents('http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=sausages');
+	// $data = json_decode($json);
+	// foreach ($data->responseData->results as $result) {
+	// 	$results[] = array('url' => $result->url, 'alt' => $result->title);
+	// }
+	// print_r($results); 
+	echo $json;
+});
