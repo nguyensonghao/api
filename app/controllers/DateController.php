c10c03085e8594f4979e0016d1377cfa0a7aae9a
nguyensonghao <nguyensonghao974@gmail.com>
2016-02-27 10:02:53 +0700

    sua lai ham check timeout

--- app/controllers/DateController.php
+++ app/controllers/DateController.php
@@ -7,7 +7,7 @@ class DateController {
 		$date = new DateTime();
 		$timeServer = $date->getTimestamp();
 	    $minus = $timeServer - $timeClient;
-	    if ($minus > 120) 
+	    if ($minus > 3000) 
 	    	return false;
 	    else 
 	    	return true;
