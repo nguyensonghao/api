27284d36c064b60025f76978034e18851e4a5e68
nguyensonghao <nguyensonghao974@gmail.com>
2016-03-18 09:28:34 +0700

    fix

--- app/filters.php
+++ app/filters.php
@@ -13,11 +13,11 @@
 
 App::before(function($request)
 {	
-	header_remove('Access-Control-Allow-Origin');
-	header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
-    header('Access-Control-Allow-Credentials: true');
+	// header_remove('Access-Control-Allow-Origin');
+	// header('Access-Control-Allow-Origin: *');
+ //    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+ //    header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+ //    header('Access-Control-Allow-Credentials: true');
 });
 
 
