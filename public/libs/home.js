ccc9634956e0cea83da39e5d46ebd826af96b2bb
nguyensonghao <nguyensonghao974@gmail.com>
2016-04-22 14:54:56 +0700

    test ham push data new

--- public/libs/home.js
+++ public/libs/home.js
@@ -13,15 +13,34 @@ angular.module('app').controller('HomeController',
 	else 
 		$scope.logined = true;
 
-    var dataSend = {
-        userId : 38,
-        timeLocal : '2015-04-21T04:06:38.790Z',
-    }
+	var string = '[{"type":"word","query":"のに","date":1461210621514,"category":4,"isRemember":0,"id":3}]';
+	var listNote = JSON.parse(string);
+    var listDataSend = [];
+    var size = listNote.length;
+    var deferred = $q.defer();
+    var urlAddNote = baseUrlApi + 'api/push-note-new';
+    for (var i = 0; i < size; i++) {
+        var note = listNote[i];            
+        if (note.type != 'grammar_detail') {
+            var idx = '-1';
+        } else {
+            var idx = note.idx;
+        }
 
-    $http.post('api/pull-note', dataSend)
-    .success(function (data) {
+        var dataSend = {
+            noteName   : note.query,
+            noteMean   : '',
+            categoryId : note.category,
+            type       : note.type,                
+            idx        : idx,
+            updated_at : new Date(note.date)
+        }
+        
+        listDataSend.push(dataSend);
+    }                
+
+    $http.post(urlAddNote, {userId : 38, listNote: JSON.stringify(listDataSend)})
+    .success(function (data) {           
         console.log(data);
     })
-    
-	
 }]);
