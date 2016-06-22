ae5cfe15373c35a233437f62d1da861508d0a1b5
nguyensonghao <nguyensonghao974@gmail.com>
2016-06-22 15:27:28 +0700

    sua lai phan giai ma token

--- app/controllers/DecodePasswordController.php
+++ app/controllers/DecodePasswordController.php
@@ -5,8 +5,7 @@ class DecodePasswordController extends BaseController {
 	public $listObjectEncryption;
 
 	public function __construct () {
-		$listEncryption = '[{"number":"46","text":"A"},{"number":"31","text":"B"},{"number":"93","text":"C"},{"number":"24","text":"D"},{"number":"37","text":"E"},{"number":"84","text":"F"},{"number":"41","text":"G"},{"number":"71","text":"H"},{"number":"16","text":"I"},{"number":"49","text":"J"},{"number":"52","text":"K"},{"number":"59","text":"L"},{"number":"45","text":"M"},{"number":"96","text":"N"},{"number":"51","text":"O"},{"number":"36","text":"P"},{"number":"95","text":"Q"},{"number":"78","text":"R"},{"number":"74","text":"S"},{"number":"19","text":"T"},{"number":"54","text":"U"},{"number":"42","text":"V"},{"number":"68","text":"W"},{"number":"91","text":"X"},{"number":"62","text":"Y"},{"number":"65","text":"Z"},{"number":"38","text":"a"},{"number":"83","text":"b"},{"number":"88","text":"c"},{"number":"21","text":"d"},{"number":"77","text":"e"},{"number":"44","text":"f"},{"number":"66","text":"g"},{"number":"32","text":"h"},{"number":"56","text":"i"},{"number":"92","text":"j"},{"number":"48","text":"k"},{"number":"34","text":"l"},{"number":"23","text":"m"},{"number":"57","text":"n"},{"number":"69","text":"o"},{"number":"27","text":"p"},{"number":"28","text":"q"},{"number":"17","text":"r"},{"number":"81","text":"s"},{"number":"14","text":"t"},{"number":"67","text":"u"},{"number":"79","text":"v"},{"number":"82","text":"w"},{"number":"26","text":"x"},{"number":"86","text":"y"},{"number":"63","text":"z"},{"number":"18","text":"0"},{"number":"89","text":"1"},{"number":"87","text":"2"},{"number":"22","text":"3"},{"number":"35","text":"4"},{"number":"58","text":"5"},{"number":"13","text":"6"},{"number":"99","text":"7"},{"number":"64","text":"8"},{"number":"55","text":"9"}]';
-		$this->listObjectEncryption = json_decode($listEncryption);
+		$listObjectEncryption = array('46' => 'A', '31' => 'B', '93' => 'C', '24' => 'D', '37' => 'E', '84' => 'F', '41' => 'G', '71' => 'H', '16' => 'I', '49' => 'J', '52' => 'K', '59' => 'L', '45' => 'M', '96' => 'N', '51' => 'O', '36' => 'P', '95' => 'Q', '78' => 'R', '74' => 'S', '19' => 'T', '54' => 'U', '42' => 'V', '68' => 'W', '91' => 'X', '62' => 'Y', '65' => 'Z', '38' => 'a', '83' => 'b', '88' => 'c', '21' => 'd', '77' => 'e', '44' => 'f', '66' => 'g', '32' => 'h', '56' => 'i', '92' => 'j', '48' => 'k', '34' => 'l', '23' => 'm', '57' => 'n', '69' => 'o', '27' => 'p', '28' => 'q', '17' => 'r', '81' => 's', '14' => 't', '67' => 'u', '79' => 'v', '82' => 'w', '26' => 'x', '86' => 'y', '63' => 'z', '18' => '0', '89' => '1', '87' => '2', '22' => '3', '35' => '4', '58' => '5', '13' => '6', '99' => '7', '64' => '8', '55' => '9');
 	}
 
 	public function decodeTokenRequest ($tokenId) {		
@@ -29,14 +28,11 @@ class DecodePasswordController extends BaseController {
 	}
 
 	public function convertStringformNumber ($number) {
-		$size = count($this->listObjectEncryption);
-		for ($i = 0; $i < $size; $i++) {
-			if ($this->listObjectEncryption[$i]->number == $number) {
-				return $this->listObjectEncryption[$i]->text;
-			}
+		try {
+			return $listObjectEncryption[$number];
+		} catch (Exception $e) {
+			return '';
 		}
-
-		return '';
 	}
 
 	public function responseUsertoClient ($user) {
